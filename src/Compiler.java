ef02de36ad745b8867a58381670731626c69f24c
cmpt220Hatch <97994991+AndrewH1031@users.noreply.github.com>
2024-01-29 00:21:12 -0500

    Create Compiler.java
    this will probably work, if I don't update soon just assume it does.

---
+++ src/Compiler.java
@@ -0,0 +1,7 @@
+public class Compiler {
+    
+    //just gonna print out a test message here for now
+    public static void main(String[] args) {
+        System.out.println("This one's for all da marbles!")
+    }
+}
