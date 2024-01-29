04e2d4cba537a01369d3990540e9e12ddf9a6898
cmpt220Hatch <97994991+AndrewH1031@users.noreply.github.com>
2024-01-29 00:25:17 -0500

    fixed some minor grammar mistakes
    I'm an idiot who forgot the semicolon key exists

--- src/Compiler.java
+++ src/Compiler.java
@@ -2,6 +2,6 @@ public class Compiler {
     
     //just gonna print out a test message here for now
     public static void main(String[] args) {
-        System.out.println("This one's for all da marbles!")
+        System.out.println("This one's for all da marbles!");
     }
 }
