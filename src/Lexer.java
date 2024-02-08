1fbe143a0c783b74f30569e00db983d6c31c5ce5
cmpt220Hatch <97994991+AndrewH1031@users.noreply.github.com>
2024-02-08 04:06:14 -0500

    Starting this Lexer thingy
    Just brainstorming this layout for now, will do more when not tired.

---
+++ src/Lexer.java
@@ -0,0 +1,22 @@
+//NOTE: Code is currently very incomplete, just messing around
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Lexer {
+
+  public static void main(String[] args) {
+
+    //Basic list stuff to handle our input and token dump
+    Scanner tokenList = new Scanner(System.in);
+    ArrayList<Tokens> = new ArrayList<>();
+    //Possible matcher here??? idk
+
+  for(int i = 0; i < tokenList.length(); i++) {
+    //Iterate through the input with scanner, pick out tokens
+      //Include special cases like $ and comment/string stuff
+    //Accomodate for errors
+    
+  }
+  }
+}
