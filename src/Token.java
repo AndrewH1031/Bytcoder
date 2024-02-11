bace1dc56a6ee2f884f4035046f16e2f01c59f68
AndrewHatch <Andrew..Hatch1@marist.edu~>
2024-02-10 19:25:50 -0500

    Full tokens list + starting on Lexer output It's also able to store the tokens it runs across, yay!

--- src/Token.java
+++ src/Token.java
@@ -3,14 +3,16 @@
 public class Token {
     public String tokenType;
     public String name;
-    public int progNum;
+    public int lineCount;
     public int lineNum;
+    public int progNum;
 
-    public Token(String tokenType, String name, int progNum, int lineNum) {
+    public Token(String tokenType, String name, int lineCount, int lineNum, int progNum) {
         this.tokenType = tokenType;
         this.name = name;
-        this.progNum = progNum;
+        this.lineCount = lineCount;
         this.lineNum = lineNum;
+        this.progNum = progNum;
         
     }
 }
